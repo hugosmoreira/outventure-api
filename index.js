02c5482dcdd431d69eeab229ed9a6d74c19ff708
Hugo Moreira <hugo.n4@gmail.com>
2023-02-17 13:47:17 -0800

    update

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
-require('dotenv').config()
+const jwt = require('jsonwebtoken');
+const dotenv = require("dotenv");
 const User = require('./models/User.js')
 const cors = require('cors');
 const app = express();
-
+const cookieParser = require('cookie-parser');
 const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = 'fasefraw4r5r3wq45wdfgw34twdfg';
+const fs = require('fs');
 
+
+dotenv.config();
 
 app.use(express.json());    // Enable JSON body parsing
+app.use(cookieParser());    // Enable cookie parsing
 app.use(cors(
   credentials = true,
   origin = 'http://127.0.0.1:5173'
@@ -19,6 +25,15 @@ mongoose.set("strictQuery", false);
 mongoose.connect(process.env.MONGO_URL);
 
 console.log(process.env.MONGO_URL);
+
+function getUserDataFromReq(req) {
+  return new Promise((resolve, reject) => {
+    jwt.verify(req.cookies.token, jwtSecret, {}, async (err, userData) => {
+      if (err) throw err;
+      resolve(userData);
+    });
+  });
+}
 
 app.get('/test', (req, res) => {
     console.log('test');
@@ -46,6 +61,57 @@ app.post('/register', async (req,res) => {
   
 });
 
+app.post('/login', async (req,res) => {
+  const {email,password} = req.body;
+  const userDoc = await User.findOne({email});
+  if (userDoc) {
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+      jwt.sign({
+        email:userDoc.email,
+        id:userDoc._id
+      }, jwtSecret, {}, (err,token) => {
+        if (err) throw err;
+        res.cookie('token', token).json(userDoc);
+      });
+    } else {
+      res.status(422).json('pass not ok');
+    }
+  } else {
+    res.json('not found');
+  }
+});
+
+app.get('/profile', (req,res) => {
+  const {token} = req.cookies;
+  if (token) {
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+      if (err) throw err;
+      const {name,email,_id} = await User.findById(userData.id);
+      res.json({name,email,_id});
+    });
+  } else {
+    res.json(null);
+  }
+});
+
+
+app.post('/places', (req,res) => {
+  const {token} = req.cookies;
+  const {
+    title,address,addedPhotos,description,price,
+    perks,extraInfo,checkIn,checkOut,maxGuests,
+  } = req.body;
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    if (err) throw err;
+    const placeDoc = await Place.create({
+      owner:userData.id,price,
+      title,address,photos:addedPhotos,description,
+      perks,extraInfo,checkIn,checkOut,maxGuests,
+    });
+    res.json(placeDoc);
+  });
+});
 
 
 app.listen(4000, '0.0.0.0' , () => {
